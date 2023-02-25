6c47130e8fe4b5c2c74a48f7dfe15540c63e196f
Natalia Kniat <natalia.kniat@nokia.com>
2023-02-25 11:45:15 +0100

    input do zbiorow

---
+++ zbiory.py
@@ -0,0 +1,13 @@
+# załóżmy, że pesel ma 4 cyfry
+# stwórzmy zbiór NFZ – ludzie, w bazie pacjentów NFZ
+# stwórzmy zbiór chorzy_rok – ludzie chorzy w ostatnim roku
+# stwórzmy zbiór chorzy_miesiac – ludzie chorzy w ostatnim miesiącu
+# stwórzmy zbiór krzyki – wszystkich ludzi mieszkających na krzykach
+# stwórzmy zbiór centrum – wszystkich ludzi mieszkających
+
+NFZ = {1234, 3476, 4544, 3423, 3254, 8769, 5436, 2345, 6532, 1243, 6435, 1298, 6732, 7688, 7648, 2345, 2356}
+chorzy_rok = set([1234, 3476, 4544, 3423, 3254, 8769, 5436])
+chorzy_miesiac = set([1234, 3476, 3098, 4544, 3423])
+krzyki = {4544, 3423, 3254, 8769, 5436, 2345, 6532, 1243}
+centrum = {7648, 2345, 2356, 3987, 1234, 3476, 3254}
+zbior_pusty = set()
